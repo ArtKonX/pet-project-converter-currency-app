83ab330eac9a6926884bd2b4f1925935a3ab030d
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-04-29 13:04:17 +0300

    Trying changed error deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: 'out'
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
