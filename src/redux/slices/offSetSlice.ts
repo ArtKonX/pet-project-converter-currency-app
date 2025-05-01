3464d6d1314567b1be8694f3450fbacf5de4140d
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-05-01 22:00:47 +0300

    Fixed bug with one item by delete all items

--- src/redux/slices/offSetSlice.ts
+++ src/redux/slices/offSetSlice.ts
@@ -36,7 +36,9 @@ export const offSetSlice = createSliceWithThunk({
             }
         }),
         removeItemInHistory: create.reducer((state, action: PayloadAction<RemoveItemInHistoryActionPayload>) => {
-            state.currenciesOffSetData = state.currenciesDataAll.filter(item => item.id !== action.payload.id);
+            state.currenciesOffSetData = state.currenciesOffSetData.filter(item => item.id !== action.payload.id);
+            state.currenciesDataAll = state.currenciesDataAll.filter(item => item.id !== action.payload.id);
+
             state.offset += 1;
         }),
         addOffset: create.reducer((state) => {
@@ -47,7 +49,7 @@ export const offSetSlice = createSliceWithThunk({
 
             state.loading = true;
 
-            const newItems = state.currenciesDataAll.slice(state.currenciesOffSetData.length + 1, state.offset);
+            const newItems = state.currenciesDataAll.slice(state.currenciesOffSetData.length, state.offset + 1);
 
             const uniqueItems = removeDuplicates([...state.currenciesOffSetData, ...newItems]);
             state.currenciesOffSetData = uniqueItems;
